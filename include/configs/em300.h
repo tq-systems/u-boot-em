4347272cd652188c434cd365b6539f42a13163d6
Christoph Krutz <christoph.krutz@tq-group.com>
2017-12-13 16:39:56 +0100

    load kernel from root file system: - EXT4 filesystem support - adjust mmcboot and bootcommand - remove obsolete tq_dsr from environment
    Signed-off-by: Christoph Krutz <christoph.krutz@tq-group.com>

--- include/configs/em300.h
+++ include/configs/em300.h
@@ -49,33 +49,54 @@
 #define CONFIG_MX28_FEC_MAC_IN_OCOTP
 #endif
 
+/* File System */
+#define CONFIG_FS_EXT4
+
 /* Boot Linux */
-#define CONFIG_BOOTFILE		"uImage"
+#define CONFIG_BOOTFILE		"zImage"
 #define CONFIG_LOADADDR		0x42000000
 #define CONFIG_SYS_LOAD_ADDR	CONFIG_LOADADDR
 
 /* Extra Environment */
 #define CONFIG_EXTRA_ENV_SETTINGS \
-	"addmisc=setenv bootargs ${bootargs} tq_dsr=${tq_dsr} panic=1\0" \
-	"addmmc=setenv bootargs ${bootargs} root=/dev/mmcblk${mmcdev}p3 rw rootwait\0" \
-	"addtty=setenv bootargs ${bootargs} console=${console_mainline},${baudrate}\0" \
-	"console_fsl=ttyAM0\0" \
-	"console_mainline=ttyAMA0\0" \
-	"erase_env=mw.b ${loadaddr} 0 512; mmc write ${loadaddr} 2 1\0" \
-	"erase_mmc=mw.b ${loadaddr} 0 512; mmc write ${loadaddr} 0 2\0" \
-	"fdtaddr=0x41000000\0" \
-	"mmcboot=run addmmc addmisc addtty; " \
-		"mmc read ${loadaddr} 8000 4000; " \
-		"mmc read ${fdtaddr} 3000 1000; " \
-		"bootz ${loadaddr} - ${fdtaddr}\0" \
+	"boot_fdt=try\0" \
 	"mmcdev=0\0" \
 	"mmcpart=2\0" \
-	"netboot=echo Booting netconsole for production process ...\0" \
-	"tq_dsr=256\0"
+	"console_fsl=ttyAM0\0" \
+	"console_mainline=ttyAMA0\0" \
+	"fdtfile=imx28-em300.dtb\0" \
+	"fdtaddr=0x41000000\0" \
+	"miscargs=setenv bootargs ${bootargs} panic=1\0" \
+	"mmcargs=setenv bootargs ${bootargs} root=/dev/mmcblk${mmcdev}p${mmcpart} rw rootwait\0" \
+	"ttyargs=setenv bootargs ${bootargs} console=${console_mainline},${baudrate}\0" \
+	"erase_env=mw.b ${loadaddr} 0 512; mmc write ${loadaddr} 2 1\0" \
+	"erase_mmc=mw.b ${loadaddr} 0 512; mmc write ${loadaddr} 0 2\0" \
+	"loadimage=ext4load mmc ${mmcdev}:${mmcpart} ${loadaddr} /boot/${bootfile}\0" \
+	"loadfdt=ext4load mmc ${mmcdev}:${mmcpart} ${fdtaddr} /boot/${fdtfile}\0" \
+	"mmcboot=echo Booting from mmc ...; " \
+		"run miscargs mmcargs ttyargs; " \
+		"if test ${boot_fdt} = yes || test ${boot_fdt} = try; then " \
+			"if run loadfdt; then " \
+				"bootz ${loadaddr} - ${fdtaddr}; " \
+			"else " \
+				"if test ${boot_fdt} = try; then " \
+					"bootz; " \
+				"else " \
+					"echo WARN: Cannot load the DT; " \
+				"fi; " \
+			"fi; " \
+		"else " \
+			"bootz; " \
+		"fi;\0" \
+	"netboot=echo Booting netconsole for production process TODO...\0"
 
 #define CONFIG_BOOTCOMMAND \
 	"mmc dev ${mmcdev} ${mmcpart}; if mmc rescan; then " \
-		"run mmcboot; " \
+		"if run loadimage; then " \
+			"run mmcboot; " \
+		"else " \
+			"run netboot; " \
+		"fi; " \
 	"else " \
 		"run netboot; " \
 	"fi;"
