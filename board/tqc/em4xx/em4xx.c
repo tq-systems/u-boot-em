aaeebcbe3d04b9d787ad7eaa66babab77cc4e193
Martin Schmiedel <martin.schmiedel@tq-group.com>
2020-08-17 08:31:24 +0000

    em4xx: initial board support
    Signed-off-by: Martin Schmiedel <martin.schmiedel@tq-group.com>

---
+++ board/tqc/em4xx/em4xx.c
@@ -0,0 +1,244 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright 2020 TQ Systems GmbH
+ */
+
+#include <common.h>
+#include <dm.h>
+#include <dm/device-internal.h>
+#include <dm/lists.h>
+#include <dm/uclass-internal.h>
+#include <asm/arch/imx8mn_pins.h>
+#include <malloc.h>
+#include <errno.h>
+#include <asm/io.h>
+#include <asm/mach-imx/iomux-v3.h>
+#include <asm-generic/gpio.h>
+#include <fsl_esdhc.h>
+#include <mmc.h>
+#include <asm/arch/sys_proto.h>
+#include <asm/mach-imx/boot_mode.h>
+#include <asm/mach-imx/gpio.h>
+#include <asm/mach-imx/mxc_i2c.h>
+#include <asm/arch/clock.h>
+#include <spl.h>
+
+#include "../common/tqc_bb.h"
+#include "../common/tqc_board_gpio.h"
+
+
+char *hw_rev_tbl[] = {
+	"REV0100",
+	"REV0101",
+	"REV0102",
+	"REV0103",
+	"REV0200",
+	"REV0201",
+	"REV0202",
+	"REV0203"
+};
+
+char *hw_ver_tbl[] = {
+	"EM4XX-CB-LLRR",
+	"EM4XX-L-CB-LRR",
+	"EM4XX-L-CB-LL",
+	"UNKNOWN",
+	"UNKNOWN.",
+	"UNKNOWN",
+	"UNKNOWN",
+	"UNKNOWN",
+	"EM4XX-CB-ULRR",
+	"EM4XX-CB-URR",
+	"EM4XX-CB-LRR",
+	"EM4XX-CB-UL",
+	"EM4XX-CB-U",
+	"EM4XX-CB-L",
+	"UNKNOWN",
+	"UNKNOWN"
+};
+
+#if !defined(CONFIG_SPL_BUILD)
+enum {
+	HW_REV0,
+	HW_REV1,
+	HW_REV2,
+	HW_VER0,
+	HW_VER1,
+	HW_VER2,
+	HW_VER3,
+	LED1_GN,
+	LED1_RD,
+	LED2_GN,
+	LED2_RD,
+	LED3_GN,
+	LED3_RD,
+	INT_5V_N,
+	FACTORY_DFLT_N,
+};
+
+static struct tqc_gpio_init_data em4xx_gid[] = {
+
+	GPIO_INIT_DATA_ENTRY(HW_REV2, "GPIO4_29", GPIOD_IS_IN),
+	GPIO_INIT_DATA_ENTRY(HW_REV1, "GPIO4_28", GPIOD_IS_IN),
+	GPIO_INIT_DATA_ENTRY(HW_REV0, "GPIO4_27", GPIOD_IS_IN),
+
+	GPIO_INIT_DATA_ENTRY(HW_VER3, "GPIO4_25", GPIOD_IS_IN),
+	GPIO_INIT_DATA_ENTRY(HW_VER2, "GPIO4_24", GPIOD_IS_IN),
+	GPIO_INIT_DATA_ENTRY(HW_VER1, "GPIO4_23", GPIOD_IS_IN),
+	GPIO_INIT_DATA_ENTRY(HW_VER0, "GPIO4_22", GPIOD_IS_IN),
+
+	GPIO_INIT_DATA_ENTRY(LED1_GN, "GPIO5_2", GPIOD_IS_OUT),
+	GPIO_INIT_DATA_ENTRY(LED1_RD, "GPIO5_3", GPIOD_IS_OUT),
+	GPIO_INIT_DATA_ENTRY(LED2_GN, "GPIO5_4", GPIOD_IS_OUT),
+	GPIO_INIT_DATA_ENTRY(LED2_RD, "GPIO5_5", GPIOD_IS_OUT),
+	GPIO_INIT_DATA_ENTRY(LED3_GN, "GPIO5_28", GPIOD_IS_OUT),
+	GPIO_INIT_DATA_ENTRY(LED3_RD, "GPIO5_29", GPIOD_IS_OUT),
+
+	GPIO_INIT_DATA_ENTRY(INT_5V_N, "GPIO5_0", GPIOD_IS_IN),
+	GPIO_INIT_DATA_ENTRY(FACTORY_DFLT_N, "GPIO4_31", GPIOD_IS_IN),
+};
+
+void print_hw_info(void)
+{
+	int idx;
+	unsigned int hw_rev = 0x0;
+	unsigned int hw_ver = 0x0;
+
+	for (idx = HW_REV0; idx <= HW_REV2; ++idx)
+		hw_rev |= (dm_gpio_get_value(&em4xx_gid[idx].desc) ? 1 : 0) <<
+			(idx - HW_REV0);
+
+	for (idx = HW_VER0; idx <= HW_VER3; ++idx)
+		hw_ver |= (dm_gpio_get_value(&em4xx_gid[idx].desc) ? 1 : 0) <<
+			(idx - HW_VER0);
+
+	printf("HW:    %s | %s\n", hw_ver_tbl[hw_ver], hw_rev_tbl[hw_rev]);
+};
+#endif
+
+#define HW_VER3_GPIO	IMX_GPIO_NR(4, 25)
+
+#define WDOG_PAD_CTRL	(PAD_CTL_DSE6 | PAD_CTL_HYS)
+
+
+static iomux_v3_cfg_t const wdog_pads[] = {
+	IMX8MN_PAD_GPIO1_IO02__WDOG1_WDOG_B | MUX_PAD_CTRL(WDOG_PAD_CTRL),
+};
+
+DECLARE_GLOBAL_DATA_PTR;
+
+
+#define UART_PAD_CTRL	(PAD_CTL_DSE6 | PAD_CTL_FSEL1)
+
+static iomux_v3_cfg_t const uart_pads[] = {
+	IMX8MN_PAD_UART1_RXD__UART1_DCE_RX | MUX_PAD_CTRL(UART_PAD_CTRL),
+	IMX8MN_PAD_UART1_TXD__UART1_DCE_TX | MUX_PAD_CTRL(UART_PAD_CTRL),
+};
+
+int board_early_init_f(void)
+{
+	struct wdog_regs *wdog = (struct wdog_regs *)WDOG1_BASE_ADDR;
+
+	imx_iomux_v3_setup_multiple_pads(wdog_pads, ARRAY_SIZE(wdog_pads));
+	set_wdog_reset(wdog);
+
+	init_uart_clk(0);
+
+	imx_iomux_v3_setup_multiple_pads(uart_pads, ARRAY_SIZE(uart_pads));
+
+	return 0;
+}
+
+int dram_init(void)
+{
+	/* rom_pointer[1] contains the size of TEE occupies */
+	if (rom_pointer[1])
+		gd->ram_size = PHYS_SDRAM_SIZE - rom_pointer[1];
+	else
+		gd->ram_size = PHYS_SDRAM_SIZE;
+
+	return 0;
+}
+
+#ifdef CONFIG_OF_BOARD_SETUP
+int ft_board_setup(void *blob, bd_t *bd)
+{
+	return 0;
+}
+#endif
+
+int board_init(void)
+{
+#if !defined(CONFIG_SPL_BUILD)
+	tqc_board_gpio_init(em4xx_gid, ARRAY_SIZE(em4xx_gid));
+	print_hw_info();
+#endif
+	return 0;
+}
+
+int print_bootinfo(void)
+{
+	enum boot_device bt_dev;
+
+	bt_dev = get_boot_device();
+
+	puts("Boot:  ");
+	switch (bt_dev) {
+	case MMC3_BOOT:
+		puts("MMC\n");
+		break;
+	case USB_BOOT:
+		puts("USB\n");
+		break;
+	default:
+		printf("Unknown/Unsupported device %u\n", bt_dev);
+		break;
+	}
+
+	return 0;
+}
+
+void adjust_env(void)
+{
+	bool has_usb = gpio_get_value(HW_VER3_GPIO);
+
+	if (!has_usb)
+		env_set("fdt_file", "imx8mn-em4xx-l.dtb");
+	else
+		env_set("fdt_file", "imx8mn-em4xx-u.dtb");
+}
+
+int board_late_init(void)
+{
+
+#ifdef CONFIG_ENV_IS_IN_MMC
+	board_late_mmc_env_init();
+#endif
+
+#if !defined(CONFIG_SPL_BUILD)
+
+	adjust_env();
+
+	/* set quartz load to 7.000 femtofarads */
+	return tqc_pcf85063_adjust_capacity(0, 0x51, 7000);
+#endif
+
+
+	return 0;
+}
+
+int checkboard(void)
+{
+	print_bootinfo();
+	return 0;
+}
+
+int mmc_map_to_kernel_blk(int dev_no)
+{
+	return dev_no;
+}
+
+int board_mmc_get_env_dev(int devno)
+{
+	return devno;
+}
---
+++ board/tqc/em4xx/spl.c
@@ -0,0 +1,225 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright 2020 TQ Systems GmbH
+ */
+#include <common.h>
+#include <errno.h>
+#include <asm/io.h>
+#include <asm/gpio.h>
+#include <asm/arch/ddr.h>
+#include <asm/arch/imx8mn_pins.h>
+#include <asm/arch/sys_proto.h>
+#include <asm/arch/clock.h>
+#include <asm/mach-imx/iomux-v3.h>
+#include <asm/mach-imx/boot_mode.h>
+#include <asm/mach-imx/gpio.h>
+#include <asm/mach-imx/mxc_i2c.h>
+#include <fsl_esdhc.h>
+#include <mmc.h>
+#include <power/pmic.h>
+#ifdef CONFIG_POWER_PCA9450
+#include <power/pca9450.h>
+#endif
+#include <spl.h>
+#include <usb.h>
+#include <usb/ehci-ci.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+#define USDHC_PAD_CTRL	(PAD_CTL_DSE6 | PAD_CTL_HYS | PAD_CTL_PUE | \
+				PAD_CTL_PE | PAD_CTL_FSEL2)
+
+#define USDHC_GPIO_PAD_CTRL (PAD_CTL_HYS | PAD_CTL_DSE1)
+
+#define HW_VER3_GPIO	IMX_GPIO_NR(4, 25)
+
+static iomux_v3_cfg_t const usdhc3_pads[] = {
+	IMX8MN_PAD_NAND_WE_B__USDHC3_CLK | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	IMX8MN_PAD_NAND_WP_B__USDHC3_CMD | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	IMX8MN_PAD_NAND_DATA04__USDHC3_DATA0 | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	IMX8MN_PAD_NAND_DATA05__USDHC3_DATA1 | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	IMX8MN_PAD_NAND_DATA06__USDHC3_DATA2 | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	IMX8MN_PAD_NAND_DATA07__USDHC3_DATA3 | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	IMX8MN_PAD_NAND_RE_B__USDHC3_DATA4 | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	IMX8MN_PAD_NAND_CE2_B__USDHC3_DATA5 | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	IMX8MN_PAD_NAND_CE3_B__USDHC3_DATA6 | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	IMX8MN_PAD_NAND_CLE__USDHC3_DATA7 | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	IMX8MN_PAD_NAND_READY_B__USDHC3_RESET_B | MUX_PAD_CTRL(USDHC_GPIO_PAD_CTRL),
+	IMX8MN_PAD_NAND_CE1_B__USDHC3_STROBE | MUX_PAD_CTRL(USDHC_GPIO_PAD_CTRL),
+};
+
+#define I2C_PAD_CTRL	(PAD_CTL_DSE6 | PAD_CTL_HYS | PAD_CTL_PUE | PAD_CTL_PE)
+
+static struct i2c_pads_info i2c_pad_info = {
+	.scl = {
+		.i2c_mode = IMX8MN_PAD_I2C1_SCL__I2C1_SCL | MUX_PAD_CTRL(I2C_PAD_CTRL),
+		.gpio_mode = IMX8MN_PAD_I2C1_SCL__GPIO5_IO14 | MUX_PAD_CTRL(I2C_PAD_CTRL),
+		.gp = IMX_GPIO_NR(5, 14),
+	},
+	.sda = {
+		.i2c_mode = IMX8MN_PAD_I2C1_SDA__I2C1_SDA | MUX_PAD_CTRL(I2C_PAD_CTRL),
+		.gpio_mode = IMX8MN_PAD_I2C1_SDA__GPIO5_IO15 | MUX_PAD_CTRL(I2C_PAD_CTRL),
+		.gp = IMX_GPIO_NR(5, 15),
+	},
+};
+
+extern struct dram_timing_info em4xx_512mb_lpddr4_timing;
+
+static void spl_dram_init(void)
+{
+	ddr_init(&em4xx_512mb_lpddr4_timing);
+};
+
+static struct fsl_esdhc_cfg usdhc3_cfg = {
+	.esdhc_base = USDHC3_BASE_ADDR,
+	.max_bus_width = 8,
+};
+
+int board_mmc_getcd(struct mmc *mmc)
+{
+	return 1;
+}
+
+int board_mmc_init(bd_t *bis)
+{
+	int ret;
+
+	debug("%s\n", __func__);
+	/*
+	 * According to the board_mmc_init() the following map is done:
+	 * (U-Boot device node)    (Physical Port)
+	 * mmc2                    USDHC3
+	 */
+	init_clk_usdhc(2);
+	usdhc3_cfg.sdhc_clk = mxc_get_clock(MXC_ESDHC3_CLK);
+	imx_iomux_v3_setup_multiple_pads(
+		usdhc3_pads, ARRAY_SIZE(usdhc3_pads));
+
+	ret = fsl_esdhc_initialize(bis, &usdhc3_cfg);
+	if (ret)
+		return ret;
+
+	return 0;
+}
+
+#if defined(CONFIG_POWER)
+
+#define I2C_PMIC	0
+
+#if defined(CONFIG_POWER_PCA9450)
+int power_init_board(void)
+{
+	struct pmic *p;
+	int ret;
+	u32 regval;
+
+	ret = power_pca9450b_init(I2C_PMIC);
+	if (ret)
+		printf("power init failed");
+	p = pmic_get("PCA9450");
+	pmic_probe(p);
+
+	pmic_reg_read(p, PCA9450_REG_DEV_ID, &regval);
+	printf("PMIC:  PCA9450 ID=0x%02x\n", regval);
+
+	/*
+	 * TODO:
+	 * check DVS for BUCK (power save with PMIC_STBY_REQ)
+	 * check VDD_SOC/dRAM -> 0.95 Volt
+	 * check VDD_SNVS_0V8 -> 0.85V
+	 * see imx8m[m,n]_evk
+	 */
+
+	/* set WDOG_B_CFG to cold reset w/o LDO1/2*/
+	pmic_reg_read(p, PCA9450_RESET_CTRL, &regval);
+	regval &= 0x3f;
+	regval |= 0x80;
+	pmic_reg_write(p, PCA9450_RESET_CTRL, regval);
+
+	return 0;
+}
+
+#endif /* CONFIG_POWER_PCA9450 */
+
+#endif /* CONFIG_POWER */
+
+void spl_board_init(void)
+{
+#ifndef CONFIG_SPL_USB_SDP_SUPPORT
+	/* Serial download mode */
+	if (is_usb_boot()) {
+		puts("Back to ROM, SDP\n");
+		restore_boot_params();
+	}
+#endif
+
+	puts("Normal Boot\n");
+}
+
+#ifdef CONFIG_SPL_LOAD_FIT
+int board_fit_config_name_match(const char *name)
+{
+	if (gpio_get_value(HW_VER3_GPIO) && !strcmp(name, "fsl-imx8mn-em4xx-u")) {
+		debug("USB Version detected: load USB devicetree\n");
+		return 0;
+	} else if (!strcmp(name, "fsl-imx8mn-em4xx-l")) {
+		debug("LAN Version detected: load LAN devicetree\n");
+		return 0;
+	} else
+		return -1;
+}
+#endif
+
+void board_init_f(ulong dummy)
+{
+	int ret;
+
+	/* Clear the BSS. */
+	memset(__bss_start, 0, __bss_end - __bss_start);
+
+	arch_cpu_init();
+
+	board_early_init_f();
+
+	timer_init();
+
+	init_uart_clk(0);
+
+	preloader_console_init();
+
+	ret = spl_init();
+	if (ret) {
+		debug("spl_init() failed: %d\n", ret);
+		hang();
+	}
+
+	enable_tzc380();
+
+	setup_i2c(0, CONFIG_SYS_I2C_SPEED, 0x7f, &i2c_pad_info);
+	power_init_board();
+
+	/* DDR initialization */
+	spl_dram_init();
+
+	board_init_r(NULL, 0);
+}
+
+/*
+ * To enable boot from MMC3 (e-MMC) we must correct the boot
+ * device settings, since the i.MX8MN CPU code will return BOOT_DEVICE_MMC2
+ * which is wrong for our board.
+ * Only one MMC is intialised by spl and BOOT_DEVICE_MMC1 is used in spl_mmc
+ * to query the index of the MMC device.
+ */
+void board_boot_order(u32 *spl_boot_list)
+{
+	enum boot_device boot_device_spl = get_boot_device();
+
+	switch (boot_device_spl) {
+	case MMC3_BOOT:
+		spl_boot_list[0] = BOOT_DEVICE_MMC1;
+		break;
+	default:
+		spl_boot_list[0] = spl_boot_device();
+	}
+}
