0754c87d0d8cb43a5d163695761fe0c299e4a9ea
Matthias Schiffer <matthias.schiffer@tq-group.com>
2022-02-02 17:04:40 +0100

    net: ti: am65-cpsw-nuss: add support for DM_ETH_PHY
    See upstream commit 6a895d039ba7 ("net: Update eQos driver and FEC driver
    to use eth phy interfaces") for a similar change in the fec_mxc driver.
    
    Signed-off-by: Matthias Schiffer <matthias.schiffer@tq-group.com>

--- drivers/net/ti/am65-cpsw-nuss.c
+++ drivers/net/ti/am65-cpsw-nuss.c
@@ -19,6 +19,7 @@
 #include <dm/pinctrl.h>
 #include <dma-uclass.h>
 #include <dm/of_access.h>
+#include <eth_phy.h>
 #include <miiphy.h>
 #include <net.h>
 #include <phy.h>
@@ -702,13 +703,23 @@ static int am65_cpsw_mdio_init(struct udevice *dev)
 	if (ret)
 		return ret;
 
-	cpsw_common->bus = cpsw_mdio_init(dev->name,
-					  cpsw_common->mdio_base,
-					  cpsw_common->bus_freq,
-					  clk_get_rate(&cpsw_common->fclk),
-					  priv->mdio_manual_mode);
+#ifdef CONFIG_DM_ETH_PHY
+	if (!cpsw_common->bus)
+		cpsw_common->bus = eth_phy_get_mdio_bus(cpsw_common->dev);
+#endif
+	if (!cpsw_common->bus)
+		cpsw_common->bus =
+			cpsw_mdio_init(dev->name,
+				       cpsw_common->mdio_base,
+				       cpsw_common->bus_freq,
+				       clk_get_rate(&cpsw_common->fclk),
+				       priv->mdio_manual_mode);
 	if (!cpsw_common->bus)
 		return -EFAULT;
+
+#ifdef CONFIG_DM_ETH_PHY
+	eth_phy_set_mdio_bus(cpsw_common->dev, cpsw_common->bus);
+#endif
 
 	return 0;
 }
@@ -832,6 +843,15 @@ static int am65_cpsw_port_probe(struct udevice *dev)
 		goto out;
 out:
 	return ret;
+}
+
+static int am65_cpsw_bind_nuss(struct udevice *dev)
+{
+#ifdef CONFIG_DM_ETH_PHY
+	eth_phy_binds_nodes(dev);
+#endif
+
+	return 0;
 }
 
 static int am65_cpsw_probe_nuss(struct udevice *dev)
@@ -965,6 +985,7 @@ U_BOOT_DRIVER(am65_cpsw_nuss) = {
 	.id	= UCLASS_MISC,
 	.of_match = am65_cpsw_nuss_ids,
 	.probe	= am65_cpsw_probe_nuss,
+	.bind	= am65_cpsw_bind_nuss,
 	.priv_auto = sizeof(struct am65_cpsw_common),
 };
 
