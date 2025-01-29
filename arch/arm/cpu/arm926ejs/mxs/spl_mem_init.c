0ffd7015a45117ec4405eae01f54919428f18548
Gregor Herburger <gregor.herburger@ew.tq-group.com>
2025-01-29 13:31:33 +0100

    arch: arm926ejs: mxs: spl_mem_init: make clock divider configurable
    Added a new Kconfig option SPL_MXS_REF_EMI_DIV to make the EMI clock
    Fractional divider configurable. This replaces the hardcoded values for
    CONFIG_MX23 and CONFIG_MX28 with a configurable option.
    
    The resulting frequency shall be 480 * (18 / EMIFRAC) where EMIFRAC is a
    value between 18 and 35. The frequency is further divided by DIV_EMI
    which is currently hardcoded to 2.
    
    Signed-off-by: Gregor Herburger <gregor.herburger@ew.tq-group.com>

--- arch/arm/cpu/arm926ejs/mxs/spl_mem_init.c
+++ arch/arm/cpu/arm926ejs/mxs/spl_mem_init.c
@@ -143,13 +143,7 @@ static void mxs_mem_init_clock(void)
 {
 	struct mxs_clkctrl_regs *clkctrl_regs =
 		(struct mxs_clkctrl_regs *)MXS_CLKCTRL_BASE;
-#if defined(CONFIG_MX23)
-	/* Fractional divider for ref_emi is 33 ; 480 * 18 / 33 = 266MHz */
-	const unsigned char divider = 33;
-#elif defined(CONFIG_MX28)
-	/* Fractional divider for ref_emi is 21 ; 480 * 18 / 21 = 411MHz */
-	const unsigned char divider = 21;
-#endif
+	const unsigned char divider = CONFIG_SPL_MXS_REF_EMI_DIV;
 
 	debug("SPL: Initialising FRAC0\n");
 
@@ -167,7 +161,6 @@ static void mxs_mem_init_clock(void)
 
 	early_delay(11000);
 
-	/* Set EMI clock divider for EMI clock to 411 / 2 = 205MHz */
 	writel((2 << CLKCTRL_EMI_DIV_EMI_OFFSET) |
 		(1 << CLKCTRL_EMI_DIV_XTAL_OFFSET),
 		&clkctrl_regs->hw_clkctrl_emi);
